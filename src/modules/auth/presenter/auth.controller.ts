52ca400404956a622691ff35236685df7454c5fe
Yerdos <qop6261@gmail.com>
2026-01-19 18:10:02 +0500

    feat: update url

--- src/modules/auth/presenter/auth.controller.ts
+++ src/modules/auth/presenter/auth.controller.ts
@@ -1,11 +1,32 @@
-import { Controller, HttpCode, HttpStatus, Post } from '@nestjs/common';
+import { Body, Controller, HttpCode, HttpStatus, Post } from '@nestjs/common';
 import { AuthService } from '../domain/auth.service';
+import { CreateUserBody } from './bodies/create-user.body';
+import { UserResource } from 'src/modules/users/resources/user.resource';
 
-@Controller('auth')
+@Controller('/v1/auth')
 export class AuthController {
-  constructor(private readonly authService: AuthService) {}
+  constructor(
+    private readonly authService: AuthService,
+    private readonly userResource: UserResource,
+  ) {}
 
-  @Post('register')
+  @Post('/register')
   @HttpCode(HttpStatus.CREATED)
-  async register() {}
+  async register(@Body() body: CreateUserBody) {
+    const [user, auth] = await this.authService.register({
+      username: body.username,
+      email: body.email,
+      firstName: body.first_name,
+      lastName: body.last_name,
+      phoneNumbers: body.phone_numbers,
+      password: body.password,
+    });
+
+    return {
+      data: {
+        user: this.userResource.convert(user),
+        auth: auth,
+      },
+    };
+  }
 }
