10011c6bef7b9abe37f314edb3ad61c531293d85
Yerdos <qop6261@gmail.com>
2026-01-21 09:37:20 +0500

    feat: add bodies validator

--- src/modules/auth/presenter/bodies/create-user.body.ts
+++ src/modules/auth/presenter/bodies/create-user.body.ts
@@ -1,8 +1,40 @@
+import {
+  IsEmail,
+  IsNotEmpty,
+  IsString,
+  IsStrongPassword,
+  MinLength,
+} from 'class-validator';
+
 export class CreateUserBody {
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(3)
   username: string;
+
+  @IsString()
+  @IsNotEmpty()
   first_name: string;
+
+  @IsString()
+  @IsNotEmpty()
   last_name: string;
+
+  @IsEmail()
+  @IsNotEmpty()
   email: string;
+
+  @IsString()
+  @IsStrongPassword({
+    minLength: 8,
+    minLowercase: 1,
+    minUppercase: 1,
+    minNumbers: 1,
+    minSymbols: 1,
+  })
   password: string;
+
+  @IsString()
+  @IsNotEmpty()
   phone_numbers: string;
 }
--- src/modules/auth/presenter/bodies/login-user.body.ts
+++ src/modules/auth/presenter/bodies/login-user.body.ts
@@ -1,4 +1,22 @@
+import {
+  IsEmail,
+  IsNotEmpty,
+  IsString,
+  IsStrongPassword,
+} from 'class-validator';
+
 export class LoginUserBody {
+  @IsEmail()
+  @IsNotEmpty()
   email: string;
+
+  @IsString()
+  @IsStrongPassword({
+    minLength: 8,
+    minLowercase: 1,
+    minUppercase: 1,
+    minNumbers: 1,
+    minSymbols: 1,
+  })
   password: string;
 }
